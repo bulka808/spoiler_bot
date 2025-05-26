2df1ddad4ef271a7e8086d2eb4916f8ef98f6d78
Ilya <ilya2008n@gmail.com>
2025-05-26 23:58:50 +0300

    добавлена обработка альбомов пока что тестовая

---
+++ Albums.py
@@ -0,0 +1,41 @@
+#сам класс альбома
+class Album:
+	def __init__(self, media_group_id: str):
+		# понятно впринципе что это
+		self.media_group_id: str = media_group_id
+		# список file_id альбома
+		self.files: list[str] = []
+	def append(self, file_id: str) -> None:
+		if file_id not in self.files:
+		    self.files.append(file_id)
+		
+# класс для хранения альбомов
+class AlbumStorage:
+	def __init__(self, limit: int):
+		self.limit: int = limit
+		self.albums: list[Album] = []
+	
+	# доабвление/получение
+	def get_or_create(self, media_group_id: str) -> Album:
+		# не даем выходить за пределы лимита
+		if len(self.albums) >= self.limit: self.albums.pop(0)
+
+		for album in self.albums:
+			if album.media_group_id == media_group_id: return album
+
+		new_album = Album(media_group_id=media_group_id)
+		self.albums.append(new_album)
+		return new_album
+	
+	# удаление
+	def remove_album(self, media_group_id: str) -> Album | None:
+		rm_album = None # заглушка
+		
+		for a in self.albums: 
+			if a.media_group_id == media_group_id:
+				rm_album = a
+
+		self.albums = [a for a in self.albums if a.media_group_id != media_group_id]
+		return rm_album
+
+		
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from Albums import * # моя недо-библиотека
+
 import asyncio
 from aiogram import Bot, Dispatcher, types, Router
 from aiogram.filters.command import Command
+from aiogram.filters import *
 from aiogram import F
+from aiogram.utils.media_group import MediaGroupBuilder
 from dotenv import load_dotenv
 from os import getenv
 
@@ -11,24 +15,44 @@ bot = Bot(token=getenv("TOKEN"))
 dp = Dispatcher()
 router = Router()
 
+Albums = AlbumStorage(limit=50)
+
+# прием альбомов
+@router.message(F.media_group_id,  or_f(F.photo, F.video))
+async def get_album(message: types.Message):
+	if message.photo or message.video:
+		album = Albums.get_or_create(message.media_group_id)
+		message_media = (message.video or message.photo[-1]).file_id
+		album.append(message_media)
+
+# отправка альбомов под спойлером
+@router.message(Command("sp"), F.reply_to_message.photo, F.media_group_id.as_("media_group_id"))
+async def group_spoiler(message: types.Message,  media_group_id: str):
+	album_builder = MediaGroupBuilder(caption=f"from: {message.reply_to_message.from_user.username or message.reply_to_message.from_user.full_name} \
+								  ({message.from_user.username or message.from_user.full_name})")
+	album = Albums.get_or_create(media_group_id=media_group_id)	
+	[album_builder.add_photo(media=photo) for photo in album.files]
+	await message.answer_media_group(media=album_builder.build())
+
+# обычная обработка сообщений
 @router.message(Command("sp"), F.reply_to_message.photo[-1].file_id.as_("photo"))
 async def spoiler(message: types.Message, photo: str):
-    if not message.reply_to_message.has_media_spoiler:
-        # шлем сообщение сос спойлером
-        await message.answer_photo(
-            photo=photo,
-            caption=f"from: {message.reply_to_message.from_user.username or message.reply_to_message.from_user.full_name} \
-            ({message.from_user.username or message.from_user.full_name})",
-            has_spoiler=True
-        )
-        # удаляем оригинальное сообщение и с командой
-        await bot.delete_message(message_id=message.reply_to_message.message_id, chat_id=message.chat.id)
-        await bot.delete_message(message_id=message.message_id, chat_id=message.chat.id)
+	if not message.reply_to_message.has_media_spoiler:
+		# шлем сообщение сос спойлером
+		await message.answer_photo(
+			photo=photo,
+			caption=f"from: {message.reply_to_message.from_user.username or message.reply_to_message.from_user.full_name} \
+			({message.from_user.username or message.from_user.full_name})",
+			has_spoiler=True
+		)
+		# удаляем оригинальное сообщение и с командой
+		await bot.delete_message(message_id=message.reply_to_message.message_id, chat_id=message.chat.id)
+		await bot.delete_message(message_id=message.message_id, chat_id=message.chat.id)
 
 
 async def main():
-    dp.include_router(router)
-    await dp.start_polling(bot)
+	dp.include_router(router)
+	await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+	asyncio.run(main())
