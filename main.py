183a5b03b06ab095f1cd3c4efa0104d5e0b308ee
Ilya <ilya2008n@gmail.com>
2025-05-26 14:58:58 +0300

    -

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ bot = Bot(token=getenv("TOKEN"))
 dp = Dispatcher()
 router = Router()
 
-@router.message(
-    Command("sp"),
-    F.reply_to_message.photo[-1].file_id.as_("photo"))
+@router.message(Command("sp"), F.reply_to_message.photo[-1].file_id.as_("photo"))
 async def spoiler(message: types.Message, photo: str):
     if not message.reply_to_message.has_media_spoiler:
+        # шлем сообщение сос спойлером
         await message.answer_photo(
             photo=photo,
             caption=f"from: {message.reply_to_message.from_user.username or message.reply_to_message.from_user.full_name} \
             ({message.from_user.username or message.from_user.full_name})",
             has_spoiler=True
         )
+        # удаляем оригинальное сообщение и с командой
         await bot.delete_message(message_id=message.reply_to_message.message_id, chat_id=message.chat.id)
         await bot.delete_message(message_id=message.message_id, chat_id=message.chat.id)
 
